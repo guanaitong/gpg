f2aaa615159ca3a9fd70e739dc5601ae4f1e3a55
august <august@c3po>
2023-01-05 11:56:44 +0800

    init

---
+++ src/main/java/com/ciicgat/Main.java
@@ -0,0 +1,10 @@
+package com.ciicgat;
+
+/**
+ * Created by August.Zhou on ${DATE} ${HOUR}:${MINUTE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
